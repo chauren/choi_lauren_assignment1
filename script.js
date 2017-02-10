4e2116743bf8dbc403f44675f40b2194dc8f0299
chauren <laurenmychoi@Laurens-MacBook-Pro.local>
2017-02-09 20:06:46 -0800

    Finished Assignment
    Finished assignment1

---
+++ script.js
@@ -0,0 +1,161 @@
+/* //STEP 1
+var someMonth;
+function theMonth;
+const currentMonth;
+var summerMonth;
+function myLibraryAFunction;
+*/
+
+/* //STEP 2
+//numeric literal expression
+10
+//string literal expression
+"Hello World!"
+//Boolean literal expression
+true
+//null literal expression
+null
+*/
+
+/* //STEP 3
+var expression1 = 10 + (x-y);
+var expression2 = "Hello " + "World!";
+*/
+
+/* //STEP 4
+var strFirstName;
+var strLastName;
+var strAddress;
+var strCity;
+var strState;
+var numZipCode;
+var numYourAge;
+var strReferralSource;
+var boolContactYou;
+*/
+
+/* //STEP 5
+var strFirstName = "Laur";
+
+var boolContactYou;
+boolContactYou = true;
+
+var strFirstName = "Laur", boolContactYou = true;
+*/
+
+/* //STEP 6
+var add = 25 + "tree";
+window.console.log(add);
+var existence = true;
+var name = "I am";
+window.console.log(existence + name);
+window.console.log(100 + existence);
+*/
+
+/* //STEP 7
+var cookie = " Oatmeal";
+window.console.log(cookieType);
+var cookieType = "chewy";
+*/
+
+/* //STEP 8
+//not valid
+var someString = 'Who once said, \"Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former.\"';
+window.console.log(someString);
+*/
+
+/* //STEP 9
+var x = null;
+window.console.log(x);
+
+var y;
+window.console.log(y);
+*/
+
+/* //STEP 10
+window.console.log(typeof "Laur");
+window.console.log(typeof 8);
+window.console.log(typeof true);
+window.console.log(typeof null);
+window.console.log(typeof exam);
+*/
+
+/* //STEP 11
+window.alert("Hello Lauren," + " welcome to the JavaScript class!");
+*/
+
+/*//STEP 12
+var name = "Lauren";
+window.alert("Hello " + name + ", welcome to the JavaScript class!");
+*/
+
+/* //STEP 13
+var name = "Lauren";
+var course = "JavaScript";
+window.alert("Hello " + name + ", welcome to the " + course + " class!");
+*/
+
+/* //STEP 14
+var name = "Lauren";
+var course = "JavaScript";
+window.alert("Hello " + name + ". \nWelcome to the " + course + " class!");
+*/
+
+/* //STEP 15
+var name = window.prompt("What is your name?");
+var course = "JavaScript";
+window.alert("Hello " + name + ". \nWelcome to the " + course + " class!");
+*/
+
+/* //STEP 16
+var name = window.prompt("What is your name?");
+var course = window.prompt("What course are you taking?");
+window.alert("Hello " + name + ". \nWelcome to the " + course + " class!");
+*/
+
+/* //STEP 17
+var x = 10;
+var y = 20;
+window.console.log(x + y);
+*/
+
+/* //STEP 18
+var x = 20;
+window.console.log(x += 20);
+*/
+
+/* //STEP 19
+var x = 20;
+window.console.log(x *= 5);
+*/
+
+/* //STEP 20
+var x = 20 % 3;
+window.console.log(x /= 1);
+*/
+
+/* //STEP 21
+var x = 5;
+var y = 12;
+window.console.log(x !== y || y > x);
+*/
+
+/* //STEP 22
+var x = 10;
+var y = 50;
+window.console.log(x > y && y > 51);
+*/
+
+/* //STEP 23
+var widget = new Object();
+window.console.log(typeof widget);
+*/
+
+/* //STEP 24
+var widget = new Object();
+window.console.log(widget instanceof Object);
+*/
+
+//STEP 25
+var widget = new Object();
+window.console.log(widget instanceof String);
